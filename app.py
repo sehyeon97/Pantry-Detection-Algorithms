4f673d75586a5e571c3c857fadec04840b9ca392
sehyeon97 <96443467+sehyeon97@users.noreply.github.com>
2023-11-12 15:57:25 -0800

    Receive and send data from and to client side

--- app.py
+++ app.py
@@ -2,11 +2,27 @@
 # python -m pip install flask
 # This will allow app.py to be run on the server
 
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
+@cross_origin()
 def home():
-    return "Hello, Flask!"
+    testVar = ""
+
+    if request.method == "POST":
+        testVar = request.get_json()
+        return request.get_json()
+
+    if request.method == "GET":
+        return testVar
+
+    return "Hello World!"
+
+
+app.run()
